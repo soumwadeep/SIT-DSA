eed6e6a5c899835ac0069c4974c45f394e03b4e0
Soumwadeep Guha <soumwadeep@gmail.com>
2023-08-28 10:33:13 +0530

    Added gitignore

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,6 @@
-public static void main(String[] args) {
-    System.out.println("Hello There!");
+class HelloWorld 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello There!");
+    }
 }
